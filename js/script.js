b00a91ee4b8de3f2172290eb3329bed30c07cfe9
Melissa Riska <98285308+melissariska@users.noreply.github.com>
2022-05-10 14:13:10 -0400

    Part 2 of project

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const symbol = function (word) {
     const symbolLetters = [];
@@ -22,7 +23,37 @@ symbol(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
+
     const guess = letterInput.value;
-    console.log(guess);
+    const goodGuess = validInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+
     letterInput.value = "";
 })
+
+const validInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter";
+    } else if (input.length > 1) {
+        message.innerText = "Please enter only one letter";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z";
+    } else {
+        return input;
+    }
+}
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You've already guessed that letter, try again!";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+}
