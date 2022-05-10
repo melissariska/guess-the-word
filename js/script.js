46d5001387256828286cae415e4631b3880e7d46
Melissa Riska <98285308+melissariska@users.noreply.github.com>
2022-05-10 13:36:05 -0400

    Part 1 of project

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const guessedLettersList = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const symbol = function (word) {
+    const symbolLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        symbolLetters.push("●");
+    }
+    wordInProgress.innerText = symbolLetters.join("");
+}
+
+symbol(word);
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+})
